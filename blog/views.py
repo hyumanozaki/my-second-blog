04e348586a8b71306ac9a4f26132d75c3d3aedcf
hyumanozaki <hyuma.nozaki@cosmotec.com.br>
2022-06-28 16:19:01 -0300

    changed 3

--- blog/views.py
+++ blog/views.py
@@ -18,8 +18,6 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login, logout
 #from django.core.mail import send_mail
 from datetime import datetime
-import numpy as np
-import pandas as pd
 
 
 def post_edit(request, pk):
